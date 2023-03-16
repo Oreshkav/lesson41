c157cfd9a288e7c4f121ca5a72d61b8885c8db69
Valentyna Kondrashenko <oreshkav@gmail.com>
2023-03-16 13:34:05 +0100

    add IncorrectWeightException

---
+++ src/IncorrectWeightException.java
@@ -0,0 +1,27 @@
+
+  //Задача 1
+  //Создайте своё собственное исключение IncorrectWeightException
+  // и унаследуйте его от IllegalArgumentException.
+  //
+  //Добавьте ему конструктор, в который можно передавать одно
+  // целое число.
+  //
+  //При этом супер-класс IllegalArgumentException должен
+  // создаваться с сообщением об ошибке вида "Вес не может быть
+  // отрицательным: [некоректный вес]"
+  //
+  //Дополните файл Pizza.java из классной работы, выбросив в
+  // конструкторе исключение IncorrectWeightException при попытке
+  // создать пиццу с отрицательной массой.
+  public class IncorrectWeightException extends IllegalAccessException {
+
+    public IncorrectWeightException(int weight) {
+      super("Введенный вес: " + weight + "должен быть целым числом!");
+    }
+
+    public IncorrectWeightException(String message) {
+      super(message);
+    }
+
+  }
+
